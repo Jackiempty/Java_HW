eb2d8e12a35db79e2710fa8cb674c6cd1cfc0d5c
Jackiempty <candypaper0123@gmail.com>
2025-04-10 00:43:46 +0900

    Finish an uncompleted version

--- HW4/src/hw4.java
+++ HW4/src/hw4.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class hw4 {
   public static void main(String[] args) {
-    int[][][] datasets = new int[10][3][];
-    int[][] dataset = new int[3][];
+    int[][][] datasets = new int[10][][];
+    int[][] dataset = new int[1000][2];
     int c = 0;
     int case_num = 0;
     Scanner s = new Scanner(System.in);
@@ -12,13 +12,13 @@ public class hw4 {
       try {
         s_line = s.nextLine();
         if (s_line == "") {
-          datasets[case_num] = copy_dataset(dataset);
+          datasets[case_num] = copy_dataset(dataset, c);
           case_num++;
           c = 0;
           continue;
         }
       } catch (Exception e) {
-        datasets[case_num] = copy_dataset(dataset);
+        datasets[case_num] = copy_dataset(dataset, c);
         case_num++;
         c = 0;
         break;
@@ -37,22 +37,58 @@ public class hw4 {
     s.close();
 
     for (int i = 0; i < case_num; i++) {
-
-      // System.out.println(get_Ans(dataset[i]));
+      // print_dataset(datasets[i]);
+      System.out.println(get_Ans(datasets[i]));
     }
   }
 
-  static char[] copy_dataset(char[] dataset) {
-    char[] result = new char[dataset.length];
-    for (int i = 0; i < dataset.length; i++) {
+  static int[][] copy_dataset(int[][] dataset, int count) {
+    int[][] result = new int[count][];
+    for (int i = 0; i < count; i++) {
+      result[i] = new int[dataset[i].length];
       result[i] = dataset[i];
     }
     return result;
   }
 
-  static int get_Ans(char[] dataset) {
+  static int get_Ans(int[][] dataset) {
     // System.out.println(dataset);
-    int result = 0;
-    
-    return result;
+    int result = 1000;
+    boolean yes_or_no = false;
+    int num = dataset[0][0];
+
+    for (int i = 1; i < dataset.length; i++) {
+      if (i + 1 >= dataset.length) {
+        if (dataset[i][1] != dataset[1][1]) {
+          yes_or_no = false;
+        }
+        if (dataset[1][1] < result) {
+          result = dataset[1][1];
+        }
+      } else {
+        if (dataset[i][1] != dataset[i + 1][0]) {
+          yes_or_no = false;
+        }
+        if (dataset[i + 1][0] < result) {
+          result = dataset[i + 1][0];
+        }
+      }
+    }
+
+    if (yes_or_no == true) {
+      return result;
+    } else {
+      return -1;
+    }
   }
+
+  static void print_dataset(int[][] dataset) {
+    for (int i = 0; i < dataset.length; i++) {
+      for (int j = 0; j < dataset[i].length; j++) {
+        System.out.print(dataset[i][j] + " ");
+      }
+      System.out.println();
+    }
+    System.out.println();
+  }
+}
